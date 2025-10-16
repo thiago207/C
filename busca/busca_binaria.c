e4cbcdf2a82016a1e1f8d5a8cd0001ee36477434
Thiago Felipe Ribeiro Brito <britoff02@gmail.com>
2025-10-16 09:41:52 -0300

    exe

--- busca/busca_binaria.c
+++ busca/busca_binaria.c
@@ -25,7 +25,7 @@ int busca_binaria(int vetor[], int tamanho_vetor, int valor_buscado) {
 int main() {
     int vetor[] = {1, 3, 5, 7, 9, 12, 15, 20};
     int tamanho_vetor = sizeof(vetor) / sizeof(vetor[0]);
-    int valor_buscado = 3;
+    int valor_buscado = 20;
     int resultado_busca;
 
     // Chamando a função
