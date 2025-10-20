9eb3d817bcc6422617be3f144b46924143a5cd12
Thiago Felipe Ribeiro Brito <britoff02@gmail.com>
2025-10-20 08:34:27 -0300

    ADD

--- exercicios_2gq/att4.c
+++ exercicios_2gq/att4.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 
-int main(){
-    float a=5 , b=2.5;
-    
+int main() {
+    float a = 5;
+    float b = 2.5;
 
+    float *p;
 
+    p = &a; 
+
+    printf("Valor de a: %.2f\n", a);
+
+    *p = 10;
+
+    printf("Novo valor de a: %.2f\n", a);
+
+    p = &b; 
+
+    printf("Valor de a: %.2f\n", b);
+
+    *p = 11.5;
+
+    printf("Novo valor de b: %.2f", b);
+
+    return 0;
 }
